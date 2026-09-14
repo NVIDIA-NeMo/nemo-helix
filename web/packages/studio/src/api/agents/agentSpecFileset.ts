NVIDIA-NeMo/nemo-platform

feat(studio): create an agent from a GitHub repository
The upload modal now takes a repository URL and an optional workspace
secret alongside the directory picker. The spec fileset is created with
a GitHub storage config rather than by uploading files, so the token
stays in the files service and the browser never holds it.

agent.yaml is read back through the files API once the fileset exists,
which is also the only way to see a private repository's contents from
here. A failure at that point rolls the fileset back, matching the
upload path.

The agent name has to be settled before the fileset can be named, so it
is prefilled from the repository or the chosen subdirectory instead of
being read out of agent.yaml the way the upload path does.

claimFileset and the rollback move to agentSpecFileset.ts so both create
paths share one implementation of who owns a spec fileset.

Widening the storage union reaches the datasets table, which keeps its
own copy of it, and the storage-backend label map.

Signed-off-by: mschwab <mschwab@nvidia.com>

web/packages/studio/src/api/agents/agentSpecFileset.ts
@@ -1,0 +1,62 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { isNotFoundError } from '@nemo/common/src/api/common/utils';
+import { agentsGetAgent } from '@nemo/sdk/generated/agents/api';
+import { filesDeleteFileset, filesRetrieveFileset } from '@nemo/sdk/generated/platform/api';
+
+/** An agent of this name already exists; its spec fileset is not ours to take. */
+export class AgentSpecFilesetConflictError extends Error {
+  constructor(public readonly filesetName: string) {
+    super(
+      `An agent named "${filesetName.replace(/-spec$/, '')}" already owns the fileset "${filesetName}". Choose a different name.`
+    );
+  }
+}
+
+/** A spec fileset with no agent behind it — an abandoned upload, or an agent since deleted. */
+export class AgentSpecFilesetOrphanError extends Error {
+  constructor(public readonly filesetName: string) {
+    super(
+      `A fileset named "${filesetName}" already exists, but no agent owns it — an upload that did not finish, or an agent that was deleted, since deleting an agent leaves its fileset behind. Replacing it discards its current contents.`
+    );
+  }
+}
+
+export const claimFileset = async (
+  workspace: string,
+  agentName: string,
+  filesetName: string,
+  replaceOrphanedFileset: boolean
+): Promise<void> => {
+  try {
+    await filesRetrieveFileset(workspace, filesetName);
+  } catch (error) {
+    // Only a 404 means the name is free; anything else leaves ownership unknown.
+    if (!isNotFoundError(error)) throw error;
+    return;
+  }
+
+  if (await agentExists(workspace, agentName)) {
+    throw new AgentSpecFilesetConflictError(filesetName);
+  }
+  if (!replaceOrphanedFileset) {
+    throw new AgentSpecFilesetOrphanError(filesetName);
+  }
+
+  await filesDeleteFileset(workspace, filesetName);
+};
+
+const agentExists = async (workspace: string, agentName: string): Promise<boolean> => {
+  try {
+    await agentsGetAgent(workspace, agentName);
+    return true;
+  } catch (error) {
+    if (!isNotFoundError(error)) throw error;
+    return false;
+  }
+};
+
+export const rollbackFileset = async (workspace: string, filesetName: string): Promise<void> => {
+  await filesDeleteFileset(workspace, filesetName).catch(() => undefined);
+};

web/packages/studio/src/api/agents/useCreateAgentFromGitHub.ts
@@ -1,0 +1,91 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { agentsCreateAgent } from '@nemo/sdk/generated/agents/api';
+import type { Agent } from '@nemo/sdk/generated/agents/schema/Agent';
+import { filesCreateFileset, filesDownloadFile } from '@nemo/sdk/generated/platform/api';
+import { claimFileset, rollbackFileset } from '@studio/api/agents/agentSpecFileset';
+import {
+  AGENT_CONFIG_FILENAME,
+  FABRIC_CONFIG_FORMAT,
+} from '@studio/routes/agents/AgentsListRoute/UploadAgentModal/const';
+import {
+  type GitHubAgentSource,
+  formatGitHubSource,
+  githubStorageConfig,
+} from '@studio/routes/agents/AgentsListRoute/UploadAgentModal/github';
+import {
+  agentSpecFilesetName,
+  parseAgentConfig,
+} from '@studio/routes/agents/AgentsListRoute/UploadAgentModal/utils';
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+
+export interface CreateAgentFromGitHubParams {
+  workspace: string;
+  name: string;
+  source: GitHubAgentSource;
+  /** Workspace secret holding a personal access token. Omit for a public repository. */
+  secretName?: string;
+  replaceOrphanedFileset?: boolean;
+}
+
+/**
+ * The fileset reads the repository directly, so agent.yaml is fetched back through the files
+ * API rather than from GitHub — the token stays in the files service and never reaches here.
+ */
+export const createAgentFromGitHub = async ({
+  workspace,
+  name,
+  source,
+  secretName,
+  replaceOrphanedFileset = false,
+}: CreateAgentFromGitHubParams): Promise<Agent> => {
+  const filesetName = agentSpecFilesetName(name);
+
+  await claimFileset(workspace, name, filesetName, replaceOrphanedFileset);
+
+  await filesCreateFileset(workspace, {
+    name: filesetName,
+    description: `Agent spec for ${name}, from ${formatGitHubSource(source)}`,
+    storage: githubStorageConfig(source, secretName),
+  });
+
+  try {
+    const config = parseAgentConfig(await readAgentConfig(workspace, filesetName, source));
+
+    return await agentsCreateAgent(workspace, {
+      name,
+      description: typeof config.description === 'string' ? config.description : '',
+      config,
+      config_format: FABRIC_CONFIG_FORMAT,
+    });
+  } catch (error) {
+    await rollbackFileset(workspace, filesetName);
+    throw error;
+  }
+};
+
+const readAgentConfig = async (
+  workspace: string,
+  filesetName: string,
+  source: GitHubAgentSource
+): Promise<string> => {
+  try {
+    const blob = await filesDownloadFile(workspace, filesetName, AGENT_CONFIG_FILENAME);
+    return await blob.text();
+  } catch (error) {
+    throw new Error(
+      `Could not read ${AGENT_CONFIG_FILENAME} from ${formatGitHubSource(source)}. ` +
+        'Check the branch and directory, and that the secret can read a private repository.',
+      { cause: error }
+    );
+  }
+};
+
+export type UseCreateAgentFromGitHubOptions = Omit<
+  UseMutationOptions<Agent, Error, CreateAgentFromGitHubParams>,
+  'mutationFn'
+>;
+
+export const useCreateAgentFromGitHub = (options?: UseCreateAgentFromGitHubOptions) =>
+  useMutation({ ...options, mutationFn: createAgentFromGitHub });

web/packages/studio/src/api/agents/useCreateAgentFromUpload.ts
@@ -1,15 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { isNotFoundError } from '@nemo/common/src/api/common/utils';
-import { agentsCreateAgent, agentsGetAgent } from '@nemo/sdk/generated/agents/api';
+import { agentsCreateAgent } from '@nemo/sdk/generated/agents/api';
 import type { Agent } from '@nemo/sdk/generated/agents/schema/Agent';
-import {
-  filesCreateFileset,
-  filesDeleteFileset,
-  filesRetrieveFileset,
-  filesUploadFile,
-} from '@nemo/sdk/generated/platform/api';
+import { filesCreateFileset, filesUploadFile } from '@nemo/sdk/generated/platform/api';
+import { claimFileset, rollbackFileset } from '@studio/api/agents/agentSpecFileset';
 import {
   AGENT_CONFIG_FILENAME,
   FABRIC_CONFIG_FORMAT,
@@ -26,24 +21,6 @@ export interface CreateAgentFromUploadParams {
   name: string;
   entries: UploadAgentEntry[];
   replaceOrphanedFileset?: boolean;
-}
-
-/** An agent of this name already exists; its spec fileset is not ours to take. */
-export class AgentSpecFilesetConflictError extends Error {
-  constructor(public readonly filesetName: string) {
-    super(
-      `An agent named "${filesetName.replace(/-spec$/, '')}" already owns the fileset "${filesetName}". Choose a different name.`
-    );
-  }
-}
-
-/** A spec fileset with no agent behind it — an abandoned upload, or an agent since deleted. */
-export class AgentSpecFilesetOrphanError extends Error {
-  constructor(public readonly filesetName: string) {
-    super(
-      `A fileset named "${filesetName}" already exists, but no agent owns it — an upload that did not finish, or an agent that was deleted, since deleting an agent leaves its fileset behind. Replacing it discards its current contents.`
-    );
-  }
 }
 
 // Files first: the fileset reserves the name, and a create-time validation that needs a
@@ -78,42 +55,8 @@ export const createAgentFromUpload = async ({
       config_format: FABRIC_CONFIG_FORMAT,
     });
   } catch (error) {
-    await rollback(workspace, filesetName);
+    await rollbackFileset(workspace, filesetName);
     throw error;
-  }
-};
-
-const claimFileset = async (
-  workspace: string,
-  agentName: string,
-  filesetName: string,
-  replaceOrphanedFileset: boolean
-): Promise<void> => {
-  try {
-    await filesRetrieveFileset(workspace, filesetName);
-  } catch (error) {
-    // Only a 404 means the name is free; anything else leaves ownership unknown.
-    if (!isNotFoundError(error)) throw error;
-    return;
-  }
-
-  if (await agentExists(workspace, agentName)) {
-    throw new AgentSpecFilesetConflictError(filesetName);
-  }
-  if (!replaceOrphanedFileset) {
-    throw new AgentSpecFilesetOrphanError(filesetName);
-  }
-
-  await filesDeleteFileset(workspace, filesetName);
-};
-
-const agentExists = async (workspace: string, agentName: string): Promise<boolean> => {
-  try {
-    await agentsGetAgent(workspace, agentName);
-    return true;
-  } catch (error) {
-    if (!isNotFoundError(error)) throw error;
-    return false;
   }
 };
 
@@ -138,10 +81,6 @@ const uploadEntries = async (
   await Promise.all(
     Array.from({ length: Math.min(UPLOAD_CONCURRENCY, entries.length) }, () => worker())
   );
-};
-
-const rollback = async (workspace: string, filesetName: string): Promise<void> => {
-  await filesDeleteFileset(workspace, filesetName).catch(() => undefined);
 };
 
 export type UseCreateAgentFromUploadOptions = Omit<

web/packages/studio/src/routes/agents/AgentsListRoute/UploadAgentModal/github.test.ts
@@ -1,0 +1,100 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  GitHubSourceError,
+  agentNameFromSource,
+  formatGitHubSource,
+  githubStorageConfig,
+  parseGitHubSource,
+} from '@studio/routes/agents/AgentsListRoute/UploadAgentModal/github';
+
+describe('parseGitHubSource', () => {
+  it.each([
+    ['owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    ['github.com/owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    ['https://github.com/owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    [
+      'https://github.com/owner/repo.git',
+      { owner: 'owner', repo: 'repo', ref: undefined, path: '' },
+    ],
+    ['git@github.com:owner/repo.git', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    [
+      'https://github.com/owner/repo/tree/main/agents/calc',
+      { owner: 'owner', repo: 'repo', ref: 'main', path: 'agents/calc' },
+    ],
+    [
+      'github.com/owner/repo@v1.2#agents/calc',
+      { owner: 'owner', repo: 'repo', ref: 'v1.2', path: 'agents/calc' },
+    ],
+  ])('parses %s', (input, expected) => {
+    expect(parseGitHubSource(input)).toEqual(expected);
+  });
+
+  it('does not read the git@ userinfo as a ref', () => {
+    expect(parseGitHubSource('git@github.com:owner/repo').ref).toBeUndefined();
+  });
+
+  it('rejects a host that is not GitHub', () => {
+    expect(() => parseGitHubSource('https://gitlab.com/owner/repo')).toThrow(GitHubSourceError);
+  });
+
+  it('rejects input that names no repository', () => {
+    expect(() => parseGitHubSource('owner')).toThrow(GitHubSourceError);
+    expect(() => parseGitHubSource('   ')).toThrow(GitHubSourceError);
+  });
+});
+
+describe('formatGitHubSource', () => {
+  it('round-trips the spec form', () => {
+    expect(formatGitHubSource({ owner: 'o', repo: 'r', ref: 'main', path: 'a/b' })).toBe(
+      'o/r@main#a/b'
+    );
+    expect(formatGitHubSource({ owner: 'o', repo: 'r', path: '' })).toBe('o/r');
+  });
+});
+
+describe('githubStorageConfig', () => {
+  it('omits the revision and path when the source names neither', () => {
+    expect(githubStorageConfig({ owner: 'o', repo: 'r', path: '' })).toEqual({
+      type: 'github',
+      owner: 'o',
+      repo: 'r',
+    });
+  });
+
+  it('carries the revision, directory, and token secret', () => {
+    expect(
+      githubStorageConfig({ owner: 'o', repo: 'r', ref: 'v1', path: 'agents/calc' }, 'github-pat')
+    ).toEqual({
+      type: 'github',
+      owner: 'o',
+      repo: 'r',
+      revision: 'v1',
+      path: 'agents/calc',
+      token_secret: 'github-pat',
+    });
+  });
+
+  it('omits the token secret for a public repository', () => {
+    expect(githubStorageConfig({ owner: 'o', repo: 'r', path: '' })).not.toHaveProperty(
+      'token_secret'
+    );
+  });
+});
+
+describe('agentNameFromSource', () => {
+  it('uses the repository when no directory is given', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: 'Calc-Agent', path: '' })).toBe('calc-agent');
+  });
+
+  it('prefers the directory holding agent.yaml', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: 'agents', path: 'agents/Calc_Bot' })).toBe(
+      'calc-bot'
+    );
+  });
+
+  it('produces a name the form schema accepts', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: '__weird__', path: '' })).toBe('weird');
+  });
+});

NVIDIA-NeMo/nemo-helix

web/packages/studio/src/api/agents/agentSpecFileset.ts
@@ -1,0 +1,62 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { isNotFoundError } from '@nemo/common/src/api/common/utils';
+import { agentsGetAgent } from '@nemo/sdk/generated/agents/agents';
+import { filesDeleteFileset, filesRetrieveFileset } from '@nemo/sdk/generated/platform/files';
+
+/** An agent of this name already exists; its spec fileset is not ours to take. */
+export class AgentSpecFilesetConflictError extends Error {
+  constructor(public readonly filesetName: string) {
+    super(
+      `An agent named "${filesetName.replace(/-ethos$/, '')}" already owns the fileset "${filesetName}". Choose a different name.`
+    );
+  }
+}
+
+/** A spec fileset with no agent behind it — an abandoned upload, or an agent since deleted. */
+export class AgentSpecFilesetOrphanError extends Error {
+  constructor(public readonly filesetName: string) {
+    super(
+      `A fileset named "${filesetName}" already exists, but no agent owns it — an upload that did not finish, or an agent that was deleted, since deleting an agent leaves its fileset behind. Replacing it discards its current contents.`
+    );
+  }
+}
+
+export const claimFileset = async (
+  workspace: string,
+  agentName: string,
+  filesetName: string,
+  replaceOrphanedFileset: boolean
+): Promise<void> => {
+  try {
+    await filesRetrieveFileset(workspace, filesetName);
+  } catch (error) {
+    // Only a 404 means the name is free; anything else leaves ownership unknown.
+    if (!isNotFoundError(error)) throw error;
+    return;
+  }
+
+  if (await agentExists(workspace, agentName)) {
+    throw new AgentSpecFilesetConflictError(filesetName);
+  }
+  if (!replaceOrphanedFileset) {
+    throw new AgentSpecFilesetOrphanError(filesetName);
+  }
+
+  await filesDeleteFileset(workspace, filesetName);
+};
+
+const agentExists = async (workspace: string, agentName: string): Promise<boolean> => {
+  try {
+    await agentsGetAgent(workspace, agentName);
+    return true;
+  } catch (error) {
+    if (!isNotFoundError(error)) throw error;
+    return false;
+  }
+};
+
+export const rollbackFileset = async (workspace: string, filesetName: string): Promise<void> => {
+  await filesDeleteFileset(workspace, filesetName).catch(() => undefined);
+};

web/packages/studio/src/api/agents/useCreateAgentFromGitHub.ts
@@ -1,0 +1,91 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { agentsCreateAgent } from '@nemo/sdk/generated/agents/agents';
+import type { Agent } from '@nemo/sdk/generated/agents/schema/Agent';
+import { filesCreateFileset, filesDownloadFile } from '@nemo/sdk/generated/platform/files';
+import { claimFileset, rollbackFileset } from '@studio/api/agents/agentSpecFileset';
+import {
+  AGENT_CONFIG_FILENAME,
+  FABRIC_CONFIG_FORMAT,
+} from '@studio/routes/agents/AgentsListRoute/NewAgentModal/const';
+import {
+  type GitHubAgentSource,
+  formatGitHubSource,
+  githubStorageConfig,
+} from '@studio/routes/agents/AgentsListRoute/NewAgentModal/github';
+import {
+  agentSpecFilesetName,
+  parseAgentConfig,
+} from '@studio/routes/agents/AgentsListRoute/NewAgentModal/utils';
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+
+export interface CreateAgentFromGitHubParams {
+  workspace: string;
+  name: string;
+  source: GitHubAgentSource;
+  /** Workspace secret holding a personal access token. Omit for a public repository. */
+  secretName?: string;
+  replaceOrphanedFileset?: boolean;
+}
+
+/**
+ * The fileset reads the repository directly, so agent.yaml is fetched back through the files
+ * API rather than from GitHub — the token stays in the files service and never reaches here.
+ */
+export const createAgentFromGitHub = async ({
+  workspace,
+  name,
+  source,
+  secretName,
+  replaceOrphanedFileset = false,
+}: CreateAgentFromGitHubParams): Promise<Agent> => {
+  const filesetName = agentSpecFilesetName(name);
+
+  await claimFileset(workspace, name, filesetName, replaceOrphanedFileset);
+
+  await filesCreateFileset(workspace, {
+    name: filesetName,
+    description: `Agent spec for ${name}, from ${formatGitHubSource(source)}`,
+    storage: githubStorageConfig(source, secretName),
+  });
+
+  try {
+    const config = parseAgentConfig(await readAgentConfig(workspace, filesetName, source));
+
+    return await agentsCreateAgent(workspace, {
+      name,
+      description: typeof config.description === 'string' ? config.description : '',
+      config,
+      config_format: FABRIC_CONFIG_FORMAT,
+    });
+  } catch (error) {
+    await rollbackFileset(workspace, filesetName);
+    throw error;
+  }
+};
+
+const readAgentConfig = async (
+  workspace: string,
+  filesetName: string,
+  source: GitHubAgentSource
+): Promise<string> => {
+  try {
+    const blob = await filesDownloadFile(workspace, filesetName, AGENT_CONFIG_FILENAME);
+    return await blob.text();
+  } catch (error) {
+    throw new Error(
+      `Could not read ${AGENT_CONFIG_FILENAME} from ${formatGitHubSource(source)}. ` +
+        'Check the branch and directory, and that the secret can read a private repository.',
+      { cause: error }
+    );
+  }
+};
+
+export type UseCreateAgentFromGitHubOptions = Omit<
+  UseMutationOptions<Agent, Error, CreateAgentFromGitHubParams>,
+  'mutationFn'
+>;
+
+export const useCreateAgentFromGitHub = (options?: UseCreateAgentFromGitHubOptions) =>
+  useMutation({ ...options, mutationFn: createAgentFromGitHub });

web/packages/studio/src/api/agents/useCreateAgentFromUpload.ts
@@ -1,15 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { isNotFoundError } from '@nemo/common/src/api/common/utils';
-import { agentsCreateAgent, agentsGetAgent } from '@nemo/sdk/generated/agents/agents';
+import { agentsCreateAgent } from '@nemo/sdk/generated/agents/agents';
 import type { Agent } from '@nemo/sdk/generated/agents/schema/Agent';
-import {
-  filesCreateFileset,
-  filesDeleteFileset,
-  filesRetrieveFileset,
-  filesUploadFile,
-} from '@nemo/sdk/generated/platform/files';
+import { filesCreateFileset, filesUploadFile } from '@nemo/sdk/generated/platform/files';
+import { claimFileset, rollbackFileset } from '@studio/api/agents/agentSpecFileset';
 import {
   AGENT_CONFIG_FILENAME,
   FABRIC_CONFIG_FORMAT,
@@ -26,24 +21,6 @@ export interface CreateAgentFromUploadParams {
   name: string;
   entries: UploadAgentEntry[];
   replaceOrphanedFileset?: boolean;
-}
-
-/** An agent of this name already exists; its spec fileset is not ours to take. */
-export class AgentSpecFilesetConflictError extends Error {
-  constructor(public readonly filesetName: string) {
-    super(
-      `An agent named "${filesetName.replace(/-ethos$/, '')}" already owns the fileset "${filesetName}". Choose a different name.`
-    );
-  }
-}
-
-/** A spec fileset with no agent behind it — an abandoned upload, or an agent since deleted. */
-export class AgentSpecFilesetOrphanError extends Error {
-  constructor(public readonly filesetName: string) {
-    super(
-      `A fileset named "${filesetName}" already exists, but no agent owns it — an upload that did not finish, or an agent that was deleted, since deleting an agent leaves its fileset behind. Replacing it discards its current contents.`
-    );
-  }
 }
 
 // Files first: the fileset reserves the name, and a create-time validation that needs a
@@ -78,42 +55,8 @@ export const createAgentFromUpload = async ({
       config_format: FABRIC_CONFIG_FORMAT,
     });
   } catch (error) {
-    await rollback(workspace, filesetName);
+    await rollbackFileset(workspace, filesetName);
     throw error;
-  }
-};
-
-const claimFileset = async (
-  workspace: string,
-  agentName: string,
-  filesetName: string,
-  replaceOrphanedFileset: boolean
-): Promise<void> => {
-  try {
-    await filesRetrieveFileset(workspace, filesetName);
-  } catch (error) {
-    // Only a 404 means the name is free; anything else leaves ownership unknown.
-    if (!isNotFoundError(error)) throw error;
-    return;
-  }
-
-  if (await agentExists(workspace, agentName)) {
-    throw new AgentSpecFilesetConflictError(filesetName);
-  }
-  if (!replaceOrphanedFileset) {
-    throw new AgentSpecFilesetOrphanError(filesetName);
-  }
-
-  await filesDeleteFileset(workspace, filesetName);
-};
-
-const agentExists = async (workspace: string, agentName: string): Promise<boolean> => {
-  try {
-    await agentsGetAgent(workspace, agentName);
-    return true;
-  } catch (error) {
-    if (!isNotFoundError(error)) throw error;
-    return false;
   }
 };
 
@@ -138,10 +81,6 @@ const uploadEntries = async (
   await Promise.all(
     Array.from({ length: Math.min(UPLOAD_CONCURRENCY, entries.length) }, () => worker())
   );
-};
-
-const rollback = async (workspace: string, filesetName: string): Promise<void> => {
-  await filesDeleteFileset(workspace, filesetName).catch(() => undefined);
 };
 
 export type UseCreateAgentFromUploadOptions = Omit<

web/packages/studio/src/routes/agents/AgentsListRoute/NewAgentModal/github.test.ts
@@ -1,0 +1,100 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  GitHubSourceError,
+  agentNameFromSource,
+  formatGitHubSource,
+  githubStorageConfig,
+  parseGitHubSource,
+} from '@studio/routes/agents/AgentsListRoute/NewAgentModal/github';
+
+describe('parseGitHubSource', () => {
+  it.each([
+    ['owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    ['github.com/owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    ['https://github.com/owner/repo', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    [
+      'https://github.com/owner/repo.git',
+      { owner: 'owner', repo: 'repo', ref: undefined, path: '' },
+    ],
+    ['git@github.com:owner/repo.git', { owner: 'owner', repo: 'repo', ref: undefined, path: '' }],
+    [
+      'https://github.com/owner/repo/tree/main/agents/calc',
+      { owner: 'owner', repo: 'repo', ref: 'main', path: 'agents/calc' },
+    ],
+    [
+      'github.com/owner/repo@v1.2#agents/calc',
+      { owner: 'owner', repo: 'repo', ref: 'v1.2', path: 'agents/calc' },
+    ],
+  ])('parses %s', (input, expected) => {
+    expect(parseGitHubSource(input)).toEqual(expected);
+  });
+
+  it('does not read the git@ userinfo as a ref', () => {
+    expect(parseGitHubSource('git@github.com:owner/repo').ref).toBeUndefined();
+  });
+
+  it('rejects a host that is not GitHub', () => {
+    expect(() => parseGitHubSource('https://gitlab.com/owner/repo')).toThrow(GitHubSourceError);
+  });
+
+  it('rejects input that names no repository', () => {
+    expect(() => parseGitHubSource('owner')).toThrow(GitHubSourceError);
+    expect(() => parseGitHubSource('   ')).toThrow(GitHubSourceError);
+  });
+});
+
+describe('formatGitHubSource', () => {
+  it('round-trips the spec form', () => {
+    expect(formatGitHubSource({ owner: 'o', repo: 'r', ref: 'main', path: 'a/b' })).toBe(
+      'o/r@main#a/b'
+    );
+    expect(formatGitHubSource({ owner: 'o', repo: 'r', path: '' })).toBe('o/r');
+  });
+});
+
+describe('githubStorageConfig', () => {
+  it('omits the revision and path when the source names neither', () => {
+    expect(githubStorageConfig({ owner: 'o', repo: 'r', path: '' })).toEqual({
+      type: 'github',
+      owner: 'o',
+      repo: 'r',
+    });
+  });
+
+  it('carries the revision, directory, and token secret', () => {
+    expect(
+      githubStorageConfig({ owner: 'o', repo: 'r', ref: 'v1', path: 'agents/calc' }, 'github-pat')
+    ).toEqual({
+      type: 'github',
+      owner: 'o',
+      repo: 'r',
+      revision: 'v1',
+      path: 'agents/calc',
+      token_secret: 'github-pat',
+    });
+  });
+
+  it('omits the token secret for a public repository', () => {
+    expect(githubStorageConfig({ owner: 'o', repo: 'r', path: '' })).not.toHaveProperty(
+      'token_secret'
+    );
+  });
+});
+
+describe('agentNameFromSource', () => {
+  it('uses the repository when no directory is given', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: 'Calc-Agent', path: '' })).toBe('calc-agent');
+  });
+
+  it('prefers the directory holding agent.yaml', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: 'agents', path: 'agents/Calc_Bot' })).toBe(
+      'calc-bot'
+    );
+  });
+
+  it('produces a name the form schema accepts', () => {
+    expect(agentNameFromSource({ owner: 'acme', repo: '__weird__', path: '' })).toBe('weird');
+  });
+});