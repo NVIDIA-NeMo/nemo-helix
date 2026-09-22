NVIDIA-NeMo/nemo-platform

feat: add the api for general compute and prompt optimization
Signed-off-by: Aaron Gabow <agabow@nvidia.com>

plugins/nemo-optimization/src/nemo_optimization/backends/optuna/backend.py
@@ -5,6 +5,7 @@
 
 from __future__ import annotations
 
+import copy
 import json
 import logging
 from typing import Any, ClassVar
@@ -12,15 +13,23 @@ from typing import Any, ClassVar
 from nemo_platform import NeMoPlatform
 from nemo_platform_plugin.job_context import JobContext
 
-from nemo_optimization.backends.optuna.atif_metadata import resolve_experiment_id
-from nemo_optimization.backends.optuna.fabric_trial import FabricTrialEvaluator
+from nemo_optimization.atif_metadata import resolve_experiment_id
 from nemo_optimization.backends.optuna.study_driver import (
     StudyDriverError,
     SyntheticTrialEvaluator,
     parse_numeric_study_config,
     run_numeric_study,
 )
+from nemo_optimization.backends.protocol import (
+    OptimizationBackendCapabilities,
+    OptimizationPhase,
+    OptimizationPhaseRequest,
+    OptimizationPhaseResult,
+    OptimizationPhaseStatus,
+)
+from nemo_optimization.candidate import CandidateEvaluationError
 from nemo_optimization.config import generate_optimize_id
+from nemo_optimization.fabric_evaluator import FabricCandidateEvaluator
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +40,9 @@ class OptunaBackend:
     """Numeric/categorical HPO via Optuna."""
 
     name: ClassVar[str] = "optuna"
+    capabilities: ClassVar[OptimizationBackendCapabilities] = OptimizationBackendCapabilities(
+        phases=(OptimizationPhase.NUMERIC,)
+    )
 
     def run_study(
         self,
@@ -39,7 +51,24 @@ class OptunaBackend:
         ctx: JobContext,
         sdk: NeMoPlatform | None = None,
     ) -> dict[str, Any]:
+        result = self.run_phase(
+            OptimizationPhaseRequest(payload=payload, phase=OptimizationPhase.NUMERIC),
+            ctx=ctx,
+            sdk=sdk,
+        )
+        return result.to_result_dict()
+
+    def run_phase(
+        self,
+        request: OptimizationPhaseRequest,
+        *,
+        ctx: JobContext,
+        sdk: NeMoPlatform | None = None,
+    ) -> OptimizationPhaseResult:
         del sdk
+        if request.phase is not OptimizationPhase.NUMERIC:
+            raise StudyDriverError(f"Optuna backend does not support the {request.phase.value!r} phase.")
+        payload = request.payload
         output_dir = ctx.storage.persistent / "results" / RESULT_NAME
         if "optimizer" not in payload:
             raise StudyDriverError("payload must include an 'optimizer' section.")
@@ -51,21 +80,29 @@ class OptunaBackend:
             raise StudyDriverError(f"payload optimizer section is missing required key: {exc}") from exc
 
         metric_names = tuple(metric.name for metric in config.metrics)
-        experiment_id = resolve_experiment_id(payload, generate_id=generate_optimize_id)
-        evaluator = _build_trial_evaluator(
-            payload,
-            metric_names=metric_names,
-            output_dir=output_dir,
-            experiment_id=experiment_id,
-        )
-
-        result = run_numeric_study(payload, output_dir, evaluator)
+        experiment_id = request.experiment_id or resolve_experiment_id(payload, generate_id=generate_optimize_id)
+        try:
+            evaluator = _build_trial_evaluator(
+                payload,
+                metric_names=metric_names,
+                output_dir=output_dir,
+                experiment_id=experiment_id,
+            )
+            result = run_numeric_study(payload, output_dir, evaluator)
+        except (CandidateEvaluationError, StudyDriverError) as exc:
+            return _failed_numeric_phase_result(
+                payload,
+                backend=self.name,
+                output_dir=output_dir,
+                ctx=ctx,
+                experiment_id=experiment_id,
+                error=str(exc),
+                trial_number_offset=request.trial_number_offset,
+            )
         summary = {
-            "status": "completed",
-            "backend": self.name,
-            "phase": "core",
             "experiment_id": experiment_id,
             "n_trials": result.n_trials,
+            "executed_trials": result.executed_trials,
             "best_trial": result.best_trial.number,
             "best_params": dict(result.best_trial.params),
             "best_values": list(result.best_trial.values or []),
@@ -73,17 +110,35 @@ class OptunaBackend:
             "agent": payload.get("metadata", {}).get("name"),
         }
         summary_path = output_dir / "study_summary.json"
-        summary_path.write_text(json.dumps(summary, indent=2) + "\n", encoding="utf-8")
+        summary_path.write_text(
+            json.dumps(
+                {
+                    "status": OptimizationPhaseStatus.COMPLETED.value,
+                    "backend": self.name,
+                    "phase": OptimizationPhase.NUMERIC.value,
+                    **summary,
+                },
+                indent=2,
+            )
+            + "\n",
+            encoding="utf-8",
+        )
         debug_path = output_dir / "study_debug.json"
         debug_path.write_text(
             json.dumps(_study_debug_payload(result), indent=2, default=str) + "\n",
             encoding="utf-8",
         )
         ref = ctx.results.save(RESULT_NAME, output_dir)
-        return {
-            **summary,
-            "result": ref.model_dump(mode="json"),
-        }
+        return OptimizationPhaseResult(
+            phase=OptimizationPhase.NUMERIC,
+            backend=self.name,
+            status=OptimizationPhaseStatus.COMPLETED,
+            optimized_payload=result.optimized_payload,
+            summary=summary,
+            artifacts={"result": ref.model_dump(mode="json")},
+            trial_count=result.executed_trials,
+            trial_number_offset=request.trial_number_offset,
+        )
 
 
 def _study_debug_payload(result) -> dict[str, Any]:
@@ -108,12 +163,45 @@ def _study_debug_payload(result) -> dict[str, Any]:
         "directions": [direction.name for direction in study.directions],
         "sampler": type(study.sampler).__name__,
         "n_trials": result.n_trials,
+        "executed_trials": result.executed_trials,
         "metric_names": list(result.metric_names),
         "best_trial": result.best_trial.number,
         "best_params": dict(result.best_trial.params),
         "best_values": list(result.best_trial.values or []),
         "trials": trials,
     }
+
+
+def _failed_numeric_phase_result(
+    payload: dict[str, Any],
+    *,
+    backend: str,
+    output_dir,
+    ctx: JobContext,
+    experiment_id: str,
+    error: str,
+    trial_number_offset: int,
+) -> OptimizationPhaseResult:
+    output_dir.mkdir(parents=True, exist_ok=True)
+    summary = {
+        "status": OptimizationPhaseStatus.FAILED.value,
+        "backend": backend,
+        "phase": OptimizationPhase.NUMERIC.value,
+        "experiment_id": experiment_id,
+        "error": error,
+    }
+    (output_dir / "numeric_phase_failure.json").write_text(json.dumps(summary, indent=2) + "\n", encoding="utf-8")
+    ref = ctx.results.save(RESULT_NAME, output_dir)
+    return OptimizationPhaseResult(
+        phase=OptimizationPhase.NUMERIC,
+        backend=backend,
+        status=OptimizationPhaseStatus.FAILED,
+        optimized_payload=copy.deepcopy(payload),
+        summary={"experiment_id": experiment_id, "error": error},
+        artifacts={"result": ref.model_dump(mode="json")},
+        trial_count=0,
+        trial_number_offset=trial_number_offset,
+    )
 
 
 def _build_trial_evaluator(
@@ -124,7 +212,7 @@ def _build_trial_evaluator(
     experiment_id: str,
 ):
     if isinstance(payload.get("eval"), dict):
-        return FabricTrialEvaluator(
+        return FabricCandidateEvaluator(
             payload=payload,
             metric_names=metric_names,
             output_dir=output_dir,

NVIDIA-NeMo/nemo-helix

plugins/nemo-optimization/src/nemo_optimization/backends/optuna/backend.py
@@ -7,20 +7,31 @@ from __future__ import annotations
 
 import json
 import logging
+from collections.abc import Mapping
 from typing import Any, ClassVar
 
 from nemo_platform_plugin.client.adapter import SyncPlatformClient
 from nemo_platform_plugin.job_context import JobContext
 
-from nemo_optimization.backends.optuna.atif_metadata import resolve_experiment_id
-from nemo_optimization.backends.optuna.fabric_trial import FabricTrialEvaluator
+from nemo_optimization.atif_metadata import resolve_experiment_id
 from nemo_optimization.backends.optuna.study_driver import (
+    NumericStudyConfig,
     StudyDriverError,
     SyntheticTrialEvaluator,
     parse_numeric_study_config,
     run_numeric_study,
 )
+from nemo_optimization.backends.protocol import (
+    OptimizationBackendCapabilities,
+    OptimizationPhase,
+    OptimizationPhaseRequest,
+    OptimizationPhaseResult,
+    OptimizationPhaseStatus,
+)
+from nemo_optimization.candidate import CandidateEvaluationError
 from nemo_optimization.config import generate_optimize_id
+from nemo_optimization.fabric_evaluator import FabricCandidateEvaluator
+from nemo_optimization.optimizer_config import OptimizerConfigError
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +42,9 @@ class OptunaBackend:
     """Numeric/categorical HPO via Optuna."""
 
     name: ClassVar[str] = "optuna"
+    capabilities: ClassVar[OptimizationBackendCapabilities] = OptimizationBackendCapabilities(
+        phases=(OptimizationPhase.NUMERIC,)
+    )
 
     def run_study(
         self,
@@ -39,51 +53,100 @@ class OptunaBackend:
         ctx: JobContext,
         sdk: SyncPlatformClient | None = None,
     ) -> dict[str, Any]:
-        del sdk
-        output_dir = ctx.storage.persistent / "results" / RESULT_NAME
-        if "optimizer" not in payload:
-            raise StudyDriverError("payload must include an 'optimizer' section.")
+        result = self.run_phase(
+            OptimizationPhaseRequest(payload=payload, phase=OptimizationPhase.NUMERIC),
+            ctx=ctx,
+            sdk=sdk,
+        )
+        return result.to_result_dict()
+
+    def validate_phase(
+        self,
+        request: OptimizationPhaseRequest,
+        *,
+        ctx: JobContext,
+        sdk: SyncPlatformClient | None = None,
+    ) -> None:
+        del ctx, sdk
+        if request.phase is not OptimizationPhase.NUMERIC:
+            raise OptimizerConfigError(
+                f"Optuna backend does not support the {request.phase.value!r} phase.", phase=request.phase.value
+            )
+        payload = request.payload
         try:
-            config = parse_numeric_study_config(payload["optimizer"])
-        except StudyDriverError:
-            raise
-        except KeyError as exc:
-            raise StudyDriverError(f"payload optimizer section is missing required key: {exc}") from exc
+            _parse_numeric_config(payload)
+        except StudyDriverError as exc:
+            raise OptimizerConfigError(str(exc), phase="numeric") from exc
+
+    def run_phase(
+        self,
+        request: OptimizationPhaseRequest,
+        *,
+        ctx: JobContext,
+        sdk: SyncPlatformClient | None = None,
+    ) -> OptimizationPhaseResult:
+        del sdk
+        if request.phase is not OptimizationPhase.NUMERIC:
+            raise StudyDriverError(f"Optuna backend does not support the {request.phase.value!r} phase.")
+        payload = request.payload
+        output_dir = ctx.storage.persistent / "results" / RESULT_NAME
+        config = _parse_numeric_config(payload)
 
         metric_names = tuple(metric.name for metric in config.metrics)
-        experiment_id = resolve_experiment_id(payload, generate_id=generate_optimize_id)
-        evaluator = _build_trial_evaluator(
-            payload,
-            metric_names=metric_names,
-            output_dir=output_dir,
-            experiment_id=experiment_id,
-        )
-
-        result = run_numeric_study(payload, output_dir, evaluator)
+        experiment_id = request.experiment_id or resolve_experiment_id(payload, generate_id=generate_optimize_id)
+        try:
+            evaluator = _build_trial_evaluator(
+                payload,
+                metric_names=metric_names,
+                output_dir=output_dir,
+                experiment_id=experiment_id,
+            )
+            result = run_numeric_study(payload, output_dir, evaluator)
+        except (CandidateEvaluationError, StudyDriverError) as exc:
+            trial_count = getattr(exc, "trial_count", 0)
+            return _failed_numeric_phase_result(
+                payload,
+                backend=self.name,
+                experiment_id=experiment_id,
+                error=str(exc),
+                trial_count=trial_count if isinstance(trial_count, int) else 0,
+                trial_number_offset=request.trial_number_offset,
+            )
         summary = {
-            "status": "completed",
-            "backend": self.name,
-            "phase": "core",
             "experiment_id": experiment_id,
             "n_trials": result.n_trials,
+            "executed_trials": result.executed_trials,
             "best_trial": result.best_trial.number,
             "best_params": dict(result.best_trial.params),
             "best_values": list(result.best_trial.values or []),
             "metric_names": list(result.metric_names),
-            "agent": payload.get("metadata", {}).get("name"),
+            "agent": payload.get("metadata", {}).get("name") if isinstance(payload.get("metadata"), dict) else None,
         }
-        summary_path = output_dir / "study_summary.json"
-        summary_path.write_text(json.dumps(summary, indent=2) + "\n", encoding="utf-8")
         debug_path = output_dir / "study_debug.json"
         debug_path.write_text(
             json.dumps(_study_debug_payload(result), indent=2, default=str) + "\n",
             encoding="utf-8",
         )
-        ref = ctx.results.save(RESULT_NAME, output_dir)
-        return {
-            **summary,
-            "result": ref.model_dump(mode="json"),
-        }
+        return OptimizationPhaseResult(
+            phase=OptimizationPhase.NUMERIC,
+            backend=self.name,
+            status=OptimizationPhaseStatus.COMPLETED,
+            optimized_payload=result.optimized_payload,
+            summary=summary,
+            trial_count=result.executed_trials,
+            trial_number_offset=request.trial_number_offset,
+        )
+
+
+def _parse_numeric_config(payload: Mapping[str, Any]) -> NumericStudyConfig:
+    if "optimizer" not in payload:
+        raise StudyDriverError("payload must include an 'optimizer' section.")
+    try:
+        return parse_numeric_study_config(payload["optimizer"])
+    except StudyDriverError:
+        raise
+    except KeyError as exc:
+        raise StudyDriverError(f"payload optimizer section is missing required key: {exc}") from exc
 
 
 def _study_debug_payload(result) -> dict[str, Any]:
@@ -108,12 +171,33 @@ def _study_debug_payload(result) -> dict[str, Any]:
         "directions": [direction.name for direction in study.directions],
         "sampler": type(study.sampler).__name__,
         "n_trials": result.n_trials,
+        "executed_trials": result.executed_trials,
         "metric_names": list(result.metric_names),
         "best_trial": result.best_trial.number,
         "best_params": dict(result.best_trial.params),
         "best_values": list(result.best_trial.values or []),
         "trials": trials,
     }
+
+
+def _failed_numeric_phase_result(
+    payload: dict[str, Any],
+    *,
+    backend: str,
+    experiment_id: str,
+    error: str,
+    trial_count: int,
+    trial_number_offset: int,
+) -> OptimizationPhaseResult:
+    return OptimizationPhaseResult(
+        phase=OptimizationPhase.NUMERIC,
+        backend=backend,
+        status=OptimizationPhaseStatus.FAILED,
+        optimized_payload=payload,
+        summary={"experiment_id": experiment_id, "error": error, "executed_trials": trial_count},
+        trial_count=trial_count,
+        trial_number_offset=trial_number_offset,
+    )
 
 
 def _build_trial_evaluator(
@@ -124,7 +208,7 @@ def _build_trial_evaluator(
     experiment_id: str,
 ):
     if isinstance(payload.get("eval"), dict):
-        return FabricTrialEvaluator(
+        return FabricCandidateEvaluator(
             payload=payload,
             metric_names=metric_names,
             output_dir=output_dir,