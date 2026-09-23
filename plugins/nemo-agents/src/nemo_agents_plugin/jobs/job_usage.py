NVIDIA-NeMo/nemo-platform

feat(agents): report job token usage
Signed-off-by: Matt Kornfield <mkornfield@nvidia.com>

plugins/nemo-agents/src/nemo_agents_plugin/jobs/job_usage.py
@@ -1,0 +1,138 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Token-usage normalization for Agents jobs."""
+
+from __future__ import annotations
+
+import json
+from collections.abc import Mapping
+from pathlib import Path
+from typing import Any
+
+from nemo_agents_plugin.improvement.traces.claude_code import extract_token_usage
+from nemo_platform_plugin.job_usage import JobTokenUsage
+
+
+def fabric_output_token_usage(output: Any) -> JobTokenUsage | None:
+    """Read provider totals from a terminal Fabric output, when present."""
+    if not isinstance(output, Mapping):
+        return None
+    usage = output.get("usage")
+    if not isinstance(usage, Mapping):
+        return None
+
+    input_tokens = _first_nonnegative_int(usage, "input_tokens", "prompt_tokens")
+    output_tokens = _first_nonnegative_int(usage, "output_tokens", "completion_tokens")
+    if input_tokens is None and output_tokens is None:
+        return None
+    return JobTokenUsage(input_tokens=input_tokens, output_tokens=output_tokens)
+
+
+def evaluation_batch_token_usage(
+    batch_dir: Path,
+    *,
+    runner: str,
+    expected_executions: int,
+) -> JobTokenUsage | None:
+    """Sum complete raw execution records without using aggregate reports."""
+    if expected_executions < 1:
+        return None
+    if runner == "harbor":
+        usages = _harbor_execution_usages(batch_dir)
+    elif runner == "nat":
+        usages = _nat_execution_usages(batch_dir)
+    else:
+        return None
+
+    if len(usages) != expected_executions or any(usage is None for usage in usages):
+        return None
+    complete = [usage for usage in usages if usage is not None]
+    return JobTokenUsage(
+        input_tokens=sum(usage.input_tokens or 0 for usage in complete),
+        output_tokens=sum(usage.output_tokens or 0 for usage in complete),
+    )
+
+
+def _harbor_execution_usages(batch_dir: Path) -> list[JobTokenUsage | None]:
+    usages: list[JobTokenUsage | None] = []
+    for job_dir in sorted(path for path in batch_dir.iterdir() if path.is_dir()):
+        trial_result = _find_harbor_trial_result(job_dir)
+        if trial_result is None:
+            usages.append(None)
+            continue
+        usages.append(_harbor_trial_usage(trial_result))
+    return usages
+
+
+def _find_harbor_trial_result(job_dir: Path) -> Path | None:
+    for result_path in sorted(job_dir.rglob("result.json")):
+        payload = _load_json_object(result_path)
+        if payload is not None and "task_name" in payload:
+            return result_path
+    return None
+
+
+def _harbor_trial_usage(result_path: Path) -> JobTokenUsage | None:
+    payload = _load_json_object(result_path)
+    if payload is None:
+        return None
+    agent_result = payload.get("agent_result")
+    if isinstance(agent_result, Mapping):
+        input_tokens = _nonnegative_int(agent_result.get("n_input_tokens"))
+        output_tokens = _nonnegative_int(agent_result.get("n_output_tokens"))
+        cache_tokens = _nonnegative_int(agent_result.get("n_cache_tokens"))
+        if input_tokens is not None and output_tokens is not None:
+            return JobTokenUsage(
+                input_tokens=input_tokens + (cache_tokens or 0),
+                output_tokens=output_tokens,
+            )
+
+    trace_usage = extract_token_usage(result_path.parent)
+    if trace_usage is None:
+        return None
+    return JobTokenUsage(
+        input_tokens=trace_usage.input_tokens + trace_usage.cache_tokens,
+        output_tokens=trace_usage.output_tokens,
+    )
+
+
+def _nat_execution_usages(batch_dir: Path) -> list[JobTokenUsage | None]:
+    usages: list[JobTokenUsage | None] = []
+    for run_dir in sorted(path for path in batch_dir.iterdir() if path.is_dir()):
+        result_path = run_dir / "result.json"
+        payload = _load_json_object(result_path)
+        if payload is None:
+            usages.append(None)
+            continue
+        metrics = payload.get("metrics")
+        if not isinstance(metrics, Mapping):
+            usages.append(None)
+            continue
+        input_tokens = _nonnegative_int(metrics.get("prompt_tokens"))
+        output_tokens = _nonnegative_int(metrics.get("completion_tokens"))
+        usages.append(
+            JobTokenUsage(input_tokens=input_tokens, output_tokens=output_tokens)
+            if input_tokens is not None and output_tokens is not None
+            else None
+        )
+    return usages
+
+
+def _load_json_object(path: Path) -> dict[str, Any] | None:
+    try:
+        payload = json.loads(path.read_text())
+    except (OSError, json.JSONDecodeError):
+        return None
+    return payload if isinstance(payload, dict) else None
+
+
+def _first_nonnegative_int(values: Mapping[str, Any], *keys: str) -> int | None:
+    for key in keys:
+        if key in values:
+            return _nonnegative_int(values[key])
+    return None
+
+
+def _nonnegative_int(value: Any) -> int | None:
+    return value if type(value) is int and value >= 0 else None

plugins/nemo-agents/tests/unit/test_execute_job.py
@@ -58,6 +58,7 @@ from nemo_platform import NeMoPlatform
 from nemo_platform_plugin.dependencies import get_entity_client, get_sdk_client
 from nemo_platform_plugin.entity_client import NemoEntityNotFoundError
 from nemo_platform_plugin.job_context import JobContext
+from nemo_platform_plugin.job_usage import LocalJobUsageReporter
 from nemo_platform_plugin.jobs.exceptions import PlatformJobCompilationError
 from nemo_platform_plugin.jobs.routes import add_job_routes
 from pydantic import ValidationError
@@ -945,6 +946,27 @@ def test_run_without_input_workdir_saves_empty_input_snapshot(ctx: JobContext) -
     payload = json.loads((ctx.storage.persistent / "results" / FABRIC_RUN_RESULT_NAME).read_text())
     assert payload["metadata"] == {"adapter_runner": "python"}
     assert payload["runtime_id"] == "runtime-1"
+
+
+def test_run_reports_terminal_fabric_token_usage(ctx: JobContext) -> None:
+    spec = ExecuteAgentStepConfig(
+        request=ExecuteAgentJobConfig(agent="calc", input="hello"),
+        agent=_resolved_agent(),
+    )
+
+    async def _invoke(request: Any) -> FabricRuntimeResult:
+        return FabricRuntimeResult(
+            status="succeeded",
+            output={"response": "done", "usage": {"input_tokens": 21, "output_tokens": 8}},
+        )
+
+    with patch("nemo_agents_plugin.jobs.execute.invoke_agent_config_request_once", _invoke):
+        ExecuteAgentJob().run(spec.model_dump(mode="json"), ctx=ctx)
+
+    assert isinstance(ctx.usage, LocalJobUsageReporter)
+    assert ctx.usage.latest is not None
+    assert ctx.usage.latest.input_tokens == 21
+    assert ctx.usage.latest.output_tokens == 8
 
 
 def test_run_threads_custom_timeout_to_fabric(ctx: JobContext) -> None:

NVIDIA-NeMo/nemo-helix

(cherry picked from commit 9dd4e16a1b386869fea9e077ead381276c6f1559)
(cherry picked from commit ca21ec411efa0df88293e34e350312390bb2b146)

plugins/nemo-agents/src/nemo_agents_plugin/jobs/job_usage.py
@@ -1,0 +1,138 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Token-usage normalization for Agents jobs."""
+
+from __future__ import annotations
+
+import json
+from collections.abc import Mapping
+from pathlib import Path
+from typing import Any
+
+from nemo_agents_plugin.improvement.traces.claude_code import extract_token_usage
+from nemo_helix_plugin.job_usage import JobTokenUsage
+
+
+def fabric_output_token_usage(output: Any) -> JobTokenUsage | None:
+    """Read provider totals from a terminal Fabric output, when present."""
+    if not isinstance(output, Mapping):
+        return None
+    usage = output.get("usage")
+    if not isinstance(usage, Mapping):
+        return None
+
+    input_tokens = _first_nonnegative_int(usage, "input_tokens", "prompt_tokens")
+    output_tokens = _first_nonnegative_int(usage, "output_tokens", "completion_tokens")
+    if input_tokens is None and output_tokens is None:
+        return None
+    return JobTokenUsage(input_tokens=input_tokens, output_tokens=output_tokens)
+
+
+def evaluation_batch_token_usage(
+    batch_dir: Path,
+    *,
+    runner: str,
+    expected_executions: int,
+) -> JobTokenUsage | None:
+    """Sum complete raw execution records without using aggregate reports."""
+    if expected_executions < 1:
+        return None
+    if runner == "harbor":
+        usages = _harbor_execution_usages(batch_dir)
+    elif runner == "nat":
+        usages = _nat_execution_usages(batch_dir)
+    else:
+        return None
+
+    if len(usages) != expected_executions or any(usage is None for usage in usages):
+        return None
+    complete = [usage for usage in usages if usage is not None]
+    return JobTokenUsage(
+        input_tokens=sum(usage.input_tokens or 0 for usage in complete),
+        output_tokens=sum(usage.output_tokens or 0 for usage in complete),
+    )
+
+
+def _harbor_execution_usages(batch_dir: Path) -> list[JobTokenUsage | None]:
+    usages: list[JobTokenUsage | None] = []
+    for job_dir in sorted(path for path in batch_dir.iterdir() if path.is_dir()):
+        trial_result = _find_harbor_trial_result(job_dir)
+        if trial_result is None:
+            usages.append(None)
+            continue
+        usages.append(_harbor_trial_usage(trial_result))
+    return usages
+
+
+def _find_harbor_trial_result(job_dir: Path) -> Path | None:
+    for result_path in sorted(job_dir.rglob("result.json")):
+        payload = _load_json_object(result_path)
+        if payload is not None and "task_name" in payload:
+            return result_path
+    return None
+
+
+def _harbor_trial_usage(result_path: Path) -> JobTokenUsage | None:
+    payload = _load_json_object(result_path)
+    if payload is None:
+        return None
+    agent_result = payload.get("agent_result")
+    if isinstance(agent_result, Mapping):
+        input_tokens = _nonnegative_int(agent_result.get("n_input_tokens"))
+        output_tokens = _nonnegative_int(agent_result.get("n_output_tokens"))
+        cache_tokens = _nonnegative_int(agent_result.get("n_cache_tokens"))
+        if input_tokens is not None and output_tokens is not None:
+            return JobTokenUsage(
+                input_tokens=input_tokens + (cache_tokens or 0),
+                output_tokens=output_tokens,
+            )
+
+    trace_usage = extract_token_usage(result_path.parent)
+    if trace_usage is None:
+        return None
+    return JobTokenUsage(
+        input_tokens=trace_usage.input_tokens + trace_usage.cache_tokens,
+        output_tokens=trace_usage.output_tokens,
+    )
+
+
+def _nat_execution_usages(batch_dir: Path) -> list[JobTokenUsage | None]:
+    usages: list[JobTokenUsage | None] = []
+    for run_dir in sorted(path for path in batch_dir.iterdir() if path.is_dir()):
+        result_path = run_dir / "result.json"
+        payload = _load_json_object(result_path)
+        if payload is None:
+            usages.append(None)
+            continue
+        metrics = payload.get("metrics")
+        if not isinstance(metrics, Mapping):
+            usages.append(None)
+            continue
+        input_tokens = _nonnegative_int(metrics.get("prompt_tokens"))
+        output_tokens = _nonnegative_int(metrics.get("completion_tokens"))
+        usages.append(
+            JobTokenUsage(input_tokens=input_tokens, output_tokens=output_tokens)
+            if input_tokens is not None and output_tokens is not None
+            else None
+        )
+    return usages
+
+
+def _load_json_object(path: Path) -> dict[str, Any] | None:
+    try:
+        payload = json.loads(path.read_text())
+    except (OSError, json.JSONDecodeError):
+        return None
+    return payload if isinstance(payload, dict) else None
+
+
+def _first_nonnegative_int(values: Mapping[str, Any], *keys: str) -> int | None:
+    for key in keys:
+        if key in values:
+            return _nonnegative_int(values[key])
+    return None
+
+
+def _nonnegative_int(value: Any) -> int | None:
+    return value if type(value) is int and value >= 0 else None

plugins/nemo-agents/tests/unit/test_execute_job.py
@@ -58,6 +58,7 @@ from nemo_agents_plugin.telemetry.intake_export import supports_intake_atif_expo
 from nemo_helix_plugin.dependencies import get_entity_client, get_sdk_client
 from nemo_helix_plugin.entity_client import NemoEntityNotFoundError
 from nemo_helix_plugin.job_context import JobContext
+from nemo_helix_plugin.job_usage import LocalJobUsageReporter
 from nemo_helix_plugin.jobs.exceptions import HelixJobCompilationError
 from nemo_helix_plugin.jobs.routes import add_job_routes
 from pydantic import ValidationError
@@ -945,6 +946,27 @@ def test_run_without_input_workdir_saves_empty_input_snapshot(ctx: JobContext) -
     payload = json.loads((ctx.storage.persistent / "results" / FABRIC_RUN_RESULT_NAME).read_text())
     assert payload["metadata"] == {"adapter_runner": "python"}
     assert payload["runtime_id"] == "runtime-1"
+
+
+def test_run_reports_terminal_fabric_token_usage(ctx: JobContext) -> None:
+    spec = ExecuteAgentStepConfig(
+        request=ExecuteAgentJobConfig(agent="calc", input="hello"),
+        agent=_resolved_agent(),
+    )
+
+    async def _invoke(request: Any) -> FabricRuntimeResult:
+        return FabricRuntimeResult(
+            status="succeeded",
+            output={"response": "done", "usage": {"input_tokens": 21, "output_tokens": 8}},
+        )
+
+    with patch("nemo_agents_plugin.jobs.execute.invoke_agent_config_request_once", _invoke):
+        ExecuteAgentJob().run(spec.model_dump(mode="json"), ctx=ctx)
+
+    assert isinstance(ctx.usage, LocalJobUsageReporter)
+    assert ctx.usage.latest is not None
+    assert ctx.usage.latest.input_tokens == 21
+    assert ctx.usage.latest.output_tokens == 8
 
 
 def test_run_threads_custom_timeout_to_fabric(ctx: JobContext) -> None: