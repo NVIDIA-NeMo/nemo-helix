NVIDIA-NeMo/nemo-platform

refactor(models): read managed deployments via the deployments API
Migrate the models deployments-plugin backend's cross-workspace
orphan-discovery list off the direct entities client and onto the
deployments-plugin HTTP consumer client (AsyncDeploymentsClient).

list_managed_deployment_names now enumerates server-role
DeploymentConfig entities through the plugin's list route
(workspace="-") rather than reaching the entity store directly. The
models service principal already carries the cross-workspace list
permission that query needs, so this is authz-neutral; it just moves the
read behind the plugin's API boundary.

The write and mutating-read paths (create, delete orchestration) stay on
the entity client for now: they mutate entities in place and use
version-checked deletes that the plugin's HTTP write API does not yet
expose. Migrating those follows in a subsequent change that grows the
plugin write surface first.

Signed-off-by: Ben McCown <bmccown@nvidia.com>

services/core/models/src/nmp/core/models/controllers/backends/deployments_plugin/backend.py
@@ -11,6 +11,7 @@ from nemo_deployments_plugin.references import deployment_config_names_referenci
 from nemo_platform import AsyncNeMoPlatform
 from nemo_platform_plugin.auth import AuthContext as DeploymentAuthContext
 from nemo_platform_plugin.client.adapter import client_from_platform
+from nemo_platform_plugin.deployments.client import AsyncDeploymentsClient
 from nemo_platform_plugin.entities.client import AsyncEntitiesClient
 from nemo_platform_plugin.entity_client import NemoEntitiesClient, NemoEntityConflictError, NemoEntityNotFoundError
 from nemo_platform_plugin.models.types import ModelDeployment, ModelDeploymentStatus
@@ -53,6 +54,7 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
     def __init__(self, nmp_sdk: AsyncNeMoPlatform, config: dict[str, Any], huggingface_model_puller: str) -> None:
         self._backend_config: DeploymentsPluginConfig | None = None
         self._entities: NemoEntitiesClient | None = None
+        self._deployments: AsyncDeploymentsClient | None = None
         self._huggingface_model_puller = huggingface_model_puller
         super().__init__(nmp_sdk, config)
 
@@ -61,12 +63,23 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
 
     def shutdown(self) -> None:
         self._entities = None
+        self._deployments = None
 
     def _entity_client(self) -> NemoEntitiesClient:
         if self._entities is None:
             sdk = get_async_platform_sdk(as_service="models", internal=True)
             self._entities = NemoEntitiesClient(client_from_platform(sdk, AsyncEntitiesClient))
         return self._entities
+
+    def _deployments_client(self) -> AsyncDeploymentsClient:
+        # Read-only consumer of the deployments-plugin HTTP API (the models service
+        # principal, same SDK seam as the entity client). Writes and mutating reads
+        # still go through the entity client until the plugin write API grows the
+        # verbs they need.
+        if self._deployments is None:
+            sdk = get_async_platform_sdk(as_service="models", internal=True)
+            self._deployments = client_from_platform(sdk, AsyncDeploymentsClient)
+        return self._deployments
 
     @property
     def _cfg(self) -> DeploymentsPluginConfig:
@@ -322,11 +335,13 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
 
         Discovers server-role DeploymentConfig entities stamped with models-controller
         ownership labels (deployments-plugin does not mirror labels onto Deployment).
+        The cross-workspace query (``workspace="-"``) relies on the models service
+        principal's cross-workspace list permission.
         """
-        result = await self._entity_client().list(DeploymentConfig, workspace="-")
+        response = await self._deployments_client().list_deployment_configs(workspace="-")
         names = {
             f"{config.labels[_DEPLOYMENT_WORKSPACE_LABEL]}/{config.labels[_DEPLOYMENT_NAME_LABEL]}"
-            for config in result.data
+            async for config in response.items()
             if config.labels.get(MODEL_MANAGED_BY_LABEL) == MODEL_MANAGED_BY_MODELS_CONTROLLER
             and config.labels.get(_MODELS_ROLE_LABEL) == "server"
             and _DEPLOYMENT_WORKSPACE_LABEL in config.labels

services/core/models/tests/unit/controllers/backends/deployments_plugin/test_backend.py
@@ -12,6 +12,7 @@ from nemo_deployments_plugin.entities import Deployment, DeploymentConfig, Volum
 from nemo_deployments_plugin.reconciler.volume_reconciler import VolumeReconciler
 from nemo_deployments_plugin.types import Endpoint
 from nemo_platform_plugin.auth import AuthContext as DeploymentAuthContext
+from nemo_platform_plugin.deployments.types import DeploymentConfig as DeploymentConfigDTO
 from nemo_platform_plugin.entity_client import NemoEntityConflictError, NemoEntityNotFoundError
 from nmp.common.config import Runtime
 from nmp.core.models.app import ModelWeightsType
@@ -665,3 +666,43 @@ async def test_create_propagates_deployment_auth_context_to_plugin_deployments()
     plugin_deployments = [entity for entity in created if isinstance(entity, Deployment)]
     assert len(plugin_deployments) == 2
     assert all(deployment.auth_context == auth_context for deployment in plugin_deployments)
+
+
+def _managed_config(workspace: str, name: str, *, role: str = "server", managed: bool = True) -> DeploymentConfigDTO:
+    """A deployments-plugin DeploymentConfig DTO with models-controller ownership labels."""
+    labels = {
+        "nmp.nvidia.com/deployment-workspace": workspace,
+        "nmp.nvidia.com/deployment-name": name,
+        "nmp.nvidia.com/models-role": role,
+    }
+    if managed:
+        labels["nmp.nvidia.com/managed-by"] = "models-controller"
+    return DeploymentConfigDTO(name=f"{name}-{role}", workspace=workspace, labels=labels)
+
+
+@pytest.mark.asyncio
+async def test_list_managed_deployment_names_filters_and_queries_all_workspaces() -> None:
+    # Server-role managed configs across two workspaces, plus rows that must be
+    # excluded: a puller-role config, and one from another controller.
+    configs = [
+        _managed_config("ws-b", "dep-2"),
+        _managed_config("ws-a", "dep-1"),
+        _managed_config("ws-a", "dep-1", role="puller"),
+        _managed_config("ws-c", "other", managed=False),
+    ]
+
+    async def _items() -> object:
+        for config in configs:
+            yield config
+
+    response = Mock()
+    response.items = Mock(return_value=_items())
+    backend = DeploymentsPluginServiceBackend(AsyncMock(), {}, "puller:latest")
+    backend.init()
+    backend._deployments = AsyncMock()
+    backend._deployments.list_deployment_configs = AsyncMock(return_value=response)
+
+    names = await backend.list_managed_deployment_names()
+
+    assert names == ["ws-a/dep-1", "ws-b/dep-2"]
+    backend._deployments.list_deployment_configs.assert_awaited_once_with(workspace="-")

NVIDIA-NeMo/nemo-helix

services/core/models/src/nhx/core/models/controllers/backends/deployments_plugin/backend.py
@@ -11,6 +11,7 @@ from nemo_deployments_plugin.references import deployment_config_names_referenci
 from nemo_helix import AsyncNeMoHelix
 from nemo_helix_plugin.auth import AuthContext as DeploymentAuthContext
 from nemo_helix_plugin.client.adapter import client_from_platform
+from nemo_helix_plugin.deployments.client import AsyncDeploymentsClient
 from nemo_helix_plugin.entities.client import AsyncEntitiesClient
 from nemo_helix_plugin.entity_client import NemoEntitiesClient, NemoEntityConflictError, NemoEntityNotFoundError
 from nemo_helix_plugin.models.types import ModelDeployment, ModelDeploymentStatus
@@ -53,6 +54,7 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
     def __init__(self, nhx_sdk: AsyncNeMoHelix, config: dict[str, Any], huggingface_model_puller: str) -> None:
         self._backend_config: DeploymentsPluginConfig | None = None
         self._entities: NemoEntitiesClient | None = None
+        self._deployments: AsyncDeploymentsClient | None = None
         self._huggingface_model_puller = huggingface_model_puller
         super().__init__(nhx_sdk, config)
 
@@ -61,12 +63,23 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
 
     def shutdown(self) -> None:
         self._entities = None
+        self._deployments = None
 
     def _entity_client(self) -> NemoEntitiesClient:
         if self._entities is None:
             sdk = get_async_platform_sdk(as_service="models", internal=True)
             self._entities = NemoEntitiesClient(client_from_platform(sdk, AsyncEntitiesClient))
         return self._entities
+
+    def _deployments_client(self) -> AsyncDeploymentsClient:
+        # Read-only consumer of the deployments-plugin HTTP API (the models service
+        # principal, same SDK seam as the entity client). Writes and mutating reads
+        # still go through the entity client until the plugin write API grows the
+        # verbs they need.
+        if self._deployments is None:
+            sdk = get_async_platform_sdk(as_service="models", internal=True)
+            self._deployments = client_from_platform(sdk, AsyncDeploymentsClient)
+        return self._deployments
 
     @property
     def _cfg(self) -> DeploymentsPluginConfig:
@@ -322,11 +335,13 @@ class DeploymentsPluginServiceBackend(ServiceBackend):
 
         Discovers server-role DeploymentConfig entities stamped with models-controller
         ownership labels (deployments-plugin does not mirror labels onto Deployment).
+        The cross-workspace query (``workspace="-"``) relies on the models service
+        principal's cross-workspace list permission.
         """
-        result = await self._entity_client().list(DeploymentConfig, workspace="-")
+        response = await self._deployments_client().list_deployment_configs(workspace="-")
         names = {
             f"{config.labels[_DEPLOYMENT_WORKSPACE_LABEL]}/{config.labels[_DEPLOYMENT_NAME_LABEL]}"
-            for config in result.data
+            async for config in response.items()
             if config.labels.get(MODEL_MANAGED_BY_LABEL) == MODEL_MANAGED_BY_MODELS_CONTROLLER
             and config.labels.get(_MODELS_ROLE_LABEL) == "server"
             and _DEPLOYMENT_WORKSPACE_LABEL in config.labels

services/core/models/tests/unit/controllers/backends/deployments_plugin/test_backend.py
@@ -12,6 +12,7 @@ from nemo_deployments_plugin.entities import Deployment, DeploymentConfig, Volum
 from nemo_deployments_plugin.reconciler.volume_reconciler import VolumeReconciler
 from nemo_deployments_plugin.types import Endpoint
 from nemo_helix_plugin.auth import AuthContext as DeploymentAuthContext
+from nemo_helix_plugin.deployments.types import DeploymentConfig as DeploymentConfigDTO
 from nemo_helix_plugin.entity_client import NemoEntityConflictError, NemoEntityNotFoundError
 from nhx.common.config import Runtime
 from nhx.core.models.app import ModelWeightsType
@@ -665,3 +666,43 @@ async def test_create_propagates_deployment_auth_context_to_plugin_deployments()
     plugin_deployments = [entity for entity in created if isinstance(entity, Deployment)]
     assert len(plugin_deployments) == 2
     assert all(deployment.auth_context == auth_context for deployment in plugin_deployments)
+
+
+def _managed_config(workspace: str, name: str, *, role: str = "server", managed: bool = True) -> DeploymentConfigDTO:
+    """A deployments-plugin DeploymentConfig DTO with models-controller ownership labels."""
+    labels = {
+        "nhx.nvidia.com/deployment-workspace": workspace,
+        "nhx.nvidia.com/deployment-name": name,
+        "nhx.nvidia.com/models-role": role,
+    }
+    if managed:
+        labels["nhx.nvidia.com/managed-by"] = "models-controller"
+    return DeploymentConfigDTO(name=f"{name}-{role}", workspace=workspace, labels=labels)
+
+
+@pytest.mark.asyncio
+async def test_list_managed_deployment_names_filters_and_queries_all_workspaces() -> None:
+    # Server-role managed configs across two workspaces, plus rows that must be
+    # excluded: a puller-role config, and one from another controller.
+    configs = [
+        _managed_config("ws-b", "dep-2"),
+        _managed_config("ws-a", "dep-1"),
+        _managed_config("ws-a", "dep-1", role="puller"),
+        _managed_config("ws-c", "other", managed=False),
+    ]
+
+    async def _items() -> object:
+        for config in configs:
+            yield config
+
+    response = Mock()
+    response.items = Mock(return_value=_items())
+    backend = DeploymentsPluginServiceBackend(AsyncMock(), {}, "puller:latest")
+    backend.init()
+    backend._deployments = AsyncMock()
+    backend._deployments.list_deployment_configs = AsyncMock(return_value=response)
+
+    names = await backend.list_managed_deployment_names()
+
+    assert names == ["ws-a/dep-1", "ws-b/dep-2"]
+    backend._deployments.list_deployment_configs.assert_awaited_once_with(workspace="-")