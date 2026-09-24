NVIDIA-NeMo/nemo-platform

feat(auth): support configurable device flow
Signed-off-by: abondarenko <abondarenko@nvidia.com>

packages/nemo_platform_ext/src/nemo_platform_ext/auth/token_provider.py
@@ -15,7 +15,7 @@ from dataclasses import dataclass, field
 import httpx
 from typing_extensions import Self
 
-from nemo_platform_ext.auth.helpers import decode_jwt_claims
+from nemo_platform_ext.auth.helpers import BearerTokenSource, decode_jwt_claims, parse_bearer_token_source
 from nemo_platform_ext.client.tls import httpx_tls_config_from_env
 
 logger = logging.getLogger(__name__)
@@ -33,10 +33,22 @@ class TokenRefreshError(RuntimeError):
         super().__init__(f"Token refresh failed: {error} - {error_description}")
 
 
+class TokenPersistenceError(RuntimeError):
+    """Raised when a rotated refresh token cannot be persisted safely."""
+
+
 def _validate_expires_in(expires_in: object) -> int | float | None:
     if isinstance(expires_in, bool):
         return None
     return expires_in if isinstance(expires_in, int | float) else None
+
+
+def _select_bearer_token(token_data: dict, source: BearerTokenSource) -> str:
+    source = parse_bearer_token_source(source)
+    token = token_data.get(source)
+    if not isinstance(token, str) or not token:
+        raise RuntimeError(f"OIDC refresh response did not include the configured {source}")
+    return token
 
 
 def refresh_token_grant(
@@ -80,14 +92,19 @@ def refresh_token_grant(
 
 @dataclass
 class TokenSet:
-    """A pair of access + refresh tokens with expiry metadata."""
+    """An API bearer + refresh token pair with expiry metadata.
+
+    ``access_token`` keeps its historical name but may contain the configured
+    ID token when that is the bearer accepted by the platform.
+    """
 
     access_token: str
     refresh_token: str | None = None
     expires_at: float | None = None
 
-    @staticmethod
+    @classmethod
     def from_access_token(
+        cls,
         access_token: str,
         refresh_token: str | None = None,
         expires_in: object = None,
@@ -100,7 +117,7 @@ class TokenSet:
         validated_expires_in = _validate_expires_in(expires_in)
         if expires_at is None and validated_expires_in is not None:
             expires_at = time.time() + float(validated_expires_in)
-        return TokenSet(
+        return cls(
             access_token=access_token,
             refresh_token=refresh_token,
             expires_at=float(expires_at) if expires_at is not None else None,
@@ -115,7 +132,7 @@ class TokenSet:
 
 @dataclass
 class OIDCTokenProvider:
-    """Provides access tokens with automatic refresh via the OAuth2 refresh_token grant.
+    """Provides API bearer tokens with automatic refresh via the OAuth2 refresh_token grant.
 
     This is the core component for SDK-level token management. It:
     - Holds the current access + refresh tokens
@@ -146,6 +163,7 @@ class OIDCTokenProvider:
     load_tokens: Callable[[], TokenSet | None] | None = None
     refresh_lock: Callable[[], AbstractContextManager[None]] | None = None
     on_tokens_refreshed: Callable[[TokenSet], None] | None = None
+    bearer_token_source: BearerTokenSource = "access_token"
     _lock: threading.Lock = field(default_factory=threading.Lock, repr=False)
 
     def get_access_token(self) -> str:
@@ -239,9 +257,11 @@ class OIDCTokenProvider:
                     certificate_authority=self.certificate_authority,
                 )
 
-            new_access_token = token_data["access_token"]
+            new_access_token = _select_bearer_token(token_data, self.bearer_token_source)
             # The IdP may rotate the refresh token.
-            new_refresh_token = token_data.get("refresh_token", self.tokens.refresh_token)
+            old_refresh_token = self.tokens.refresh_token
+            new_refresh_token = token_data.get("refresh_token", old_refresh_token)
+            refresh_token_rotated = new_refresh_token != old_refresh_token
 
             self.tokens = TokenSet.from_access_token(
                 new_access_token,
@@ -253,7 +273,14 @@ class OIDCTokenProvider:
             if self.on_tokens_refreshed:
                 try:
                     self.on_tokens_refreshed(self.tokens)
-                except Exception:
+                except Exception as exc:
+                    if refresh_token_rotated:
+                        # The previous refresh token may already be invalid. Do not hide
+                        # a failure to persist its replacement.
+                        raise TokenPersistenceError(
+                            "The identity provider rotated the refresh token, but the refreshed credentials "
+                            "could not be saved. Re-authenticate before attempting another refresh."
+                        ) from exc
                     logger.warning("Failed to persist refreshed tokens", exc_info=True)
 
     def force_refresh(self) -> str:

packages/nemo_platform_ext/tests/auth/test_utils.py
@@ -120,8 +120,13 @@ class TestDiscoverNmpConfig:
             "oidc": {
                 "issuer": "https://idp.example.com",
                 "client_id": "nmp-app",
+                "cli_client_id": "nmp-cli",
+                "bearer_token_source": "id_token",
                 "token_endpoint": "https://idp.example.com/api/login/oauth/access_token",
                 "device_authorization_endpoint": "https://idp.example.com/api/login/oauth/device/code",
+                "device_authorization_requires_device_id": True,
+                "device_authorization_display_name": "NeMo Platform CLI",
+                "device_token_request_includes_scope": False,
                 "default_scopes": "openid profile",
                 "scope_prefix": "api://nmp/",
             },
@@ -132,8 +137,13 @@ class TestDiscoverNmpConfig:
             auth_enabled=True,
             issuer="https://idp.example.com",
             client_id="nmp-app",
+            cli_client_id="nmp-cli",
+            bearer_token_source="id_token",
             token_endpoint="https://idp.example.com/api/login/oauth/access_token",
             device_authorization_endpoint="https://idp.example.com/api/login/oauth/device/code",
+            device_authorization_requires_device_id=True,
+            device_authorization_display_name="NeMo Platform CLI",
+            device_token_request_includes_scope=False,
             default_scopes="openid profile",
             scope_prefix="api://nmp/",
         )
@@ -144,6 +154,14 @@ class TestDiscoverNmpConfig:
         assert result.auth_enabled is False
         assert result.client_id is None
         assert result.token_endpoint is None
+
+    def test_rejects_unknown_bearer_token_source(self, httpserver: HTTPServer):
+        httpserver.expect_request("/apis/auth/discovery").respond_with_json(
+            {"auth_enabled": True, "oidc": {"bearer_token_source": "refresh_token"}}
+        )
+
+        with pytest.raises(ValueError, match="bearer_token_source"):
+            discover_nmp_config(httpserver.url_for(""))
 
     def test_handles_missing_oidc_key(self, httpserver: HTTPServer):
         httpserver.expect_request("/apis/auth/discovery").respond_with_json({"auth_enabled": True})

packages/nmp_common/src/nmp/common/config/base.py
@@ -91,13 +91,19 @@ class OIDCConfig(BaseSettings):
 
     client_id: str = Field(
         default="",
-        description="OAuth client ID for this NeMo Platform deployment. Used for device flow and token audience validation.",
+        description="OAuth client ID for this NeMo Platform deployment and its default interactive login flows.",
+    )
+
+    cli_client_id: str | None = Field(
+        default=None,
+        description="OAuth client ID for interactive CLI user authentication. Defaults to client_id when unset.",
     )
 
     bearer_token_source: Literal["access_token", "id_token"] = Field(
         default="access_token",
-        description="OIDC user token Studio sends to NeMo Platform APIs. Use 'access_token' for standard OAuth "
-        "resource access, or 'id_token' only when the provider documents its signed ID token as the backend bearer.",
+        description="Token returned by the identity provider that clients send to NeMo Platform APIs. "
+        "Use 'access_token' for standard OAuth resource access, or 'id_token' only when the provider "
+        "documents its signed ID token as the backend bearer.",
     )
 
     # Optional: Override endpoints if not using standard discovery
@@ -114,6 +120,23 @@ class OIDCConfig(BaseSettings):
     device_authorization_endpoint: str | None = Field(
         default=None,
         description="Override device authorization endpoint (defaults to discovery).",
+    )
+
+    device_authorization_requires_device_id: bool = Field(
+        default=False,
+        description="Include a stable, locally generated device_id parameter in CLI device authorization requests. "
+        "Enable only for identity providers that require this extension.",
+    )
+
+    device_authorization_display_name: str | None = Field(
+        default=None,
+        description="Optional display_name sent with CLI device authorization requests.",
+    )
+
+    device_token_request_includes_scope: bool = Field(
+        default=True,
+        description="Include the requested scope in CLI device-flow token polling requests. "
+        "Disable for identity providers whose token endpoint rejects this extension.",
     )
 
     jwks_uri: str | None = Field(

services/core/auth/src/nmp/core/auth/api/v2/discovery/endpoints.py
@@ -5,6 +5,7 @@
 
 import logging
 import time
+from typing import Literal
 
 import httpx
 from fastapi import APIRouter, Request
@@ -31,6 +32,11 @@ class OIDCDiscoveryResponse(BaseModel):
     device_authorization_endpoint: str | None = None
     userinfo_endpoint: str | None = None
     client_id: str
+    cli_client_id: str | None = None
+    bearer_token_source: Literal["access_token", "id_token"] = "access_token"
+    device_authorization_requires_device_id: bool = False
+    device_authorization_display_name: str | None = None
+    device_token_request_includes_scope: bool = True
     default_scopes: str = "openid profile email offline_access"
     scope_prefix: str | None = None
     workload_token_exchange_enabled: bool = False
@@ -115,6 +121,11 @@ need to authenticate with this NeMo Platform deployment.
   - `device_authorization_endpoint`: Device flow authorization endpoint (for CLI)
   - `userinfo_endpoint`: UserInfo endpoint
   - `client_id`: OAuth client ID to use
+  - `cli_client_id`: Optional OAuth client ID dedicated to interactive CLI authentication
+  - `bearer_token_source`: Token response field clients send to NeMo Platform APIs
+  - `device_authorization_requires_device_id`: Whether CLI device requests must include a generated `device_id`
+  - `device_authorization_display_name`: Optional device name shown during device authorization
+  - `device_token_request_includes_scope`: Whether CLI device token requests include the requested scopes
   - `default_scopes`: OAuth scopes to request during authentication
   - `scope_prefix`: Prefix to prepend to custom scopes (those with ':' or '.default')
   - `workload_token_exchange_enabled`: Whether SDK workload identity token exchange is enabled
@@ -150,6 +161,11 @@ async def get_auth_discovery(request: Request | None = None) -> AuthDiscoveryRes
             ),
             userinfo_endpoint=config.oidc.userinfo_endpoint or discovery.get("userinfo_endpoint"),
             client_id=config.oidc.client_id,
+            cli_client_id=config.oidc.cli_client_id,
+            bearer_token_source=config.oidc.bearer_token_source,
+            device_authorization_requires_device_id=config.oidc.device_authorization_requires_device_id,
+            device_authorization_display_name=config.oidc.device_authorization_display_name,
+            device_token_request_includes_scope=config.oidc.device_token_request_includes_scope,
             default_scopes=config.oidc.default_scopes,
             scope_prefix=config.oidc.scope_prefix,
             workload_token_exchange_enabled=config.oidc.workload_token_exchange_enabled,

NVIDIA-NeMo/nemo-helix

packages/nemo_helix_ext/src/nemo_helix_ext/auth/token_provider.py
@@ -15,7 +15,7 @@ from dataclasses import dataclass, field
 import httpx
 from typing_extensions import Self
 
-from nemo_helix_ext.auth.helpers import decode_jwt_claims
+from nemo_helix_ext.auth.helpers import BearerTokenSource, decode_jwt_claims, parse_bearer_token_source
 from nemo_helix_ext.client.tls import httpx_tls_config_from_env
 
 logger = logging.getLogger(__name__)
@@ -33,10 +33,22 @@ class TokenRefreshError(RuntimeError):
         super().__init__(f"Token refresh failed: {error} - {error_description}")
 
 
+class TokenPersistenceError(RuntimeError):
+    """Raised when a rotated refresh token cannot be persisted safely."""
+
+
 def _validate_expires_in(expires_in: object) -> int | float | None:
     if isinstance(expires_in, bool):
         return None
     return expires_in if isinstance(expires_in, int | float) else None
+
+
+def _select_bearer_token(token_data: dict, source: BearerTokenSource) -> str:
+    source = parse_bearer_token_source(source)
+    token = token_data.get(source)
+    if not isinstance(token, str) or not token:
+        raise RuntimeError(f"OIDC refresh response did not include the configured {source}")
+    return token
 
 
 def refresh_token_grant(
@@ -80,14 +92,19 @@ def refresh_token_grant(
 
 @dataclass
 class TokenSet:
-    """A pair of access + refresh tokens with expiry metadata."""
+    """An API bearer + refresh token pair with expiry metadata.
+
+    ``access_token`` keeps its historical name but may contain the configured
+    ID token when that is the bearer accepted by the platform.
+    """
 
     access_token: str
     refresh_token: str | None = None
     expires_at: float | None = None
 
-    @staticmethod
+    @classmethod
     def from_access_token(
+        cls,
         access_token: str,
         refresh_token: str | None = None,
         expires_in: object = None,
@@ -100,7 +117,7 @@ class TokenSet:
         validated_expires_in = _validate_expires_in(expires_in)
         if expires_at is None and validated_expires_in is not None:
             expires_at = time.time() + float(validated_expires_in)
-        return TokenSet(
+        return cls(
             access_token=access_token,
             refresh_token=refresh_token,
             expires_at=float(expires_at) if expires_at is not None else None,
@@ -115,7 +132,7 @@ class TokenSet:
 
 @dataclass
 class OIDCTokenProvider:
-    """Provides access tokens with automatic refresh via the OAuth2 refresh_token grant.
+    """Provides API bearer tokens with automatic refresh via the OAuth2 refresh_token grant.
 
     This is the core component for SDK-level token management. It:
     - Holds the current access + refresh tokens
@@ -146,6 +163,7 @@ class OIDCTokenProvider:
     load_tokens: Callable[[], TokenSet | None] | None = None
     refresh_lock: Callable[[], AbstractContextManager[None]] | None = None
     on_tokens_refreshed: Callable[[TokenSet], None] | None = None
+    bearer_token_source: BearerTokenSource = "access_token"
     _lock: threading.Lock = field(default_factory=threading.Lock, repr=False)
 
     def get_access_token(self) -> str:
@@ -239,9 +257,11 @@ class OIDCTokenProvider:
                     certificate_authority=self.certificate_authority,
                 )
 
-            new_access_token = token_data["access_token"]
+            new_access_token = _select_bearer_token(token_data, self.bearer_token_source)
             # The IdP may rotate the refresh token.
-            new_refresh_token = token_data.get("refresh_token", self.tokens.refresh_token)
+            old_refresh_token = self.tokens.refresh_token
+            new_refresh_token = token_data.get("refresh_token", old_refresh_token)
+            refresh_token_rotated = new_refresh_token != old_refresh_token
 
             self.tokens = TokenSet.from_access_token(
                 new_access_token,
@@ -253,7 +273,14 @@ class OIDCTokenProvider:
             if self.on_tokens_refreshed:
                 try:
                     self.on_tokens_refreshed(self.tokens)
-                except Exception:
+                except Exception as exc:
+                    if refresh_token_rotated:
+                        # The previous refresh token may already be invalid. Do not hide
+                        # a failure to persist its replacement.
+                        raise TokenPersistenceError(
+                            "The identity provider rotated the refresh token, but the refreshed credentials "
+                            "could not be saved. Re-authenticate before attempting another refresh."
+                        ) from exc
                     logger.warning("Failed to persist refreshed tokens", exc_info=True)
 
     def force_refresh(self) -> str:

packages/nemo_helix_ext/tests/auth/test_utils.py
@@ -120,8 +120,13 @@ class TestDiscoverNhxConfig:
             "oidc": {
                 "issuer": "https://idp.example.com",
                 "client_id": "nhx-app",
+                "cli_client_id": "nhx-cli",
+                "bearer_token_source": "id_token",
                 "token_endpoint": "https://idp.example.com/api/login/oauth/access_token",
                 "device_authorization_endpoint": "https://idp.example.com/api/login/oauth/device/code",
+                "device_authorization_requires_device_id": True,
+                "device_authorization_display_name": "NeMo Helix CLI",
+                "device_token_request_includes_scope": False,
                 "default_scopes": "openid profile",
                 "scope_prefix": "api://nhx/",
             },
@@ -132,8 +137,13 @@ class TestDiscoverNhxConfig:
             auth_enabled=True,
             issuer="https://idp.example.com",
             client_id="nhx-app",
+            cli_client_id="nhx-cli",
+            bearer_token_source="id_token",
             token_endpoint="https://idp.example.com/api/login/oauth/access_token",
             device_authorization_endpoint="https://idp.example.com/api/login/oauth/device/code",
+            device_authorization_requires_device_id=True,
+            device_authorization_display_name="NeMo Helix CLI",
+            device_token_request_includes_scope=False,
             default_scopes="openid profile",
             scope_prefix="api://nhx/",
         )
@@ -144,6 +154,14 @@ class TestDiscoverNhxConfig:
         assert result.auth_enabled is False
         assert result.client_id is None
         assert result.token_endpoint is None
+
+    def test_rejects_unknown_bearer_token_source(self, httpserver: HTTPServer):
+        httpserver.expect_request("/apis/auth/discovery").respond_with_json(
+            {"auth_enabled": True, "oidc": {"bearer_token_source": "refresh_token"}}
+        )
+
+        with pytest.raises(ValueError, match="bearer_token_source"):
+            discover_nhx_config(httpserver.url_for(""))
 
     def test_handles_missing_oidc_key(self, httpserver: HTTPServer):
         httpserver.expect_request("/apis/auth/discovery").respond_with_json({"auth_enabled": True})

packages/nhx_common/src/nhx/common/config/base.py
@@ -91,13 +91,19 @@ class OIDCConfig(BaseSettings):
 
     client_id: str = Field(
         default="",
-        description="OAuth client ID for this NeMo Helix deployment. Used for device flow and token audience validation.",
+        description="OAuth client ID for this NeMo Helix deployment and its default interactive login flows.",
+    )
+
+    cli_client_id: str | None = Field(
+        default=None,
+        description="OAuth client ID for interactive CLI user authentication. Defaults to client_id when unset.",
     )
 
     bearer_token_source: Literal["access_token", "id_token"] = Field(
         default="access_token",
-        description="OIDC user token Studio sends to NeMo Helix APIs. Use 'access_token' for standard OAuth "
-        "resource access, or 'id_token' only when the provider documents its signed ID token as the backend bearer.",
+        description="Token returned by the identity provider that clients send to NeMo Helix APIs. "
+        "Use 'access_token' for standard OAuth resource access, or 'id_token' only when the provider "
+        "documents its signed ID token as the backend bearer.",
     )
 
     # Optional: Override endpoints if not using standard discovery
@@ -114,6 +120,23 @@ class OIDCConfig(BaseSettings):
     device_authorization_endpoint: str | None = Field(
         default=None,
         description="Override device authorization endpoint (defaults to discovery).",
+    )
+
+    device_authorization_requires_device_id: bool = Field(
+        default=False,
+        description="Include a stable, locally generated device_id parameter in CLI device authorization requests. "
+        "Enable only for identity providers that require this extension.",
+    )
+
+    device_authorization_display_name: str | None = Field(
+        default=None,
+        description="Optional display_name sent with CLI device authorization requests.",
+    )
+
+    device_token_request_includes_scope: bool = Field(
+        default=True,
+        description="Include the requested scope in CLI device-flow token polling requests. "
+        "Disable for identity providers whose token endpoint rejects this extension.",
     )
 
     jwks_uri: str | None = Field(

services/core/auth/src/nhx/core/auth/api/v2/discovery/endpoints.py
@@ -5,6 +5,7 @@
 
 import logging
 import time
+from typing import Literal
 
 import httpx
 from fastapi import APIRouter, Request
@@ -31,6 +32,11 @@ class OIDCDiscoveryResponse(BaseModel):
     device_authorization_endpoint: str | None = None
     userinfo_endpoint: str | None = None
     client_id: str
+    cli_client_id: str | None = None
+    bearer_token_source: Literal["access_token", "id_token"] = "access_token"
+    device_authorization_requires_device_id: bool = False
+    device_authorization_display_name: str | None = None
+    device_token_request_includes_scope: bool = True
     default_scopes: str = "openid profile email offline_access"
     scope_prefix: str | None = None
     workload_token_exchange_enabled: bool = False
@@ -115,6 +121,11 @@ need to authenticate with this NeMo Helix deployment.
   - `device_authorization_endpoint`: Device flow authorization endpoint (for CLI)
   - `userinfo_endpoint`: UserInfo endpoint
   - `client_id`: OAuth client ID to use
+  - `cli_client_id`: Optional OAuth client ID dedicated to interactive CLI authentication
+  - `bearer_token_source`: Token response field clients send to NeMo Helix APIs
+  - `device_authorization_requires_device_id`: Whether CLI device requests must include a generated `device_id`
+  - `device_authorization_display_name`: Optional device name shown during device authorization
+  - `device_token_request_includes_scope`: Whether CLI device token requests include the requested scopes
   - `default_scopes`: OAuth scopes to request during authentication
   - `scope_prefix`: Prefix to prepend to custom scopes (those with ':' or '.default')
   - `workload_token_exchange_enabled`: Whether SDK workload identity token exchange is enabled
@@ -150,6 +161,11 @@ async def get_auth_discovery(request: Request | None = None) -> AuthDiscoveryRes
             ),
             userinfo_endpoint=config.oidc.userinfo_endpoint or discovery.get("userinfo_endpoint"),
             client_id=config.oidc.client_id,
+            cli_client_id=config.oidc.cli_client_id,
+            bearer_token_source=config.oidc.bearer_token_source,
+            device_authorization_requires_device_id=config.oidc.device_authorization_requires_device_id,
+            device_authorization_display_name=config.oidc.device_authorization_display_name,
+            device_token_request_includes_scope=config.oidc.device_token_request_includes_scope,
             default_scopes=config.oidc.default_scopes,
             scope_prefix=config.oidc.scope_prefix,
             workload_token_exchange_enabled=config.oidc.workload_token_exchange_enabled,